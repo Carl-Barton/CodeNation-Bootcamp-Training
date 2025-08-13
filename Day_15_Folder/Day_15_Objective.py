c2f67a95c8c2d3b70957f8fbb7e2143e45236295
CarlRJBarton <carl.barton.artwork@outlook.com>
2025-08-13 21:19:34 +0100

    Adding Day 15 Folder

---
+++ Day_15_Folder/Day_15_Objective.py
@@ -0,0 +1,23 @@
+# I'm writing the objective for this project here in this file, since I'm still getting the hang of HTML 
+# and I don't want to mess something up in the files now that they work
+# 
+
+# Objective: Create a one page website about yourself.
+# Include:
+# Your Name
+# A paragraph about yourself
+# A section detailing your skills
+# Three images of a place you'd like to visit and why
+
+# List at least 3 of your favourites based some of these subjects:
+# Past Times
+# Songs
+# Movies
+# Places to visit
+# Historical figures
+# Sports teams
+
+# That being said our instructor had no problem us changing the subjects, so I choose these:
+# Video Games 
+# Movies 
+# Places to visit
