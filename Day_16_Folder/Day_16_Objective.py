05592424cc592dc8ae33272128c481a3064b72b2
CarlRJBarton <carl.barton.artwork@outlook.com>
2025-08-13 22:13:45 +0100

    Adding Day 16 Folder

---
+++ Day_16_Folder/Day_16_Objective.py
@@ -0,0 +1,16 @@
+# Day 16 1st Activity is mentioned at the bottom of Day_15_Objective.py as it's more relevant to that activity/project than to this one.
+
+# Day 16 2nd Activity:
+# Using the things you have learned yesterday, create your own unique motivational poster
+# to spread positivity around to others. Must include an image, a big main title centred underneath
+# with a paragraph centred below that.
+
+# Requirements:
+# The main h1 text should be the Regular Libre Baskerville font from Google fonts
+# The text should be white and background black
+# Image should have a 5px white solid border
+# The text shoul be centred aligned
+# Use margins to add space around your content
+
+# Day 16 3rd Activity:
+# Create a webpage gallery to present your 5 motivational memes, which you need to create as well.
